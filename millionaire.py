36fd2a697053fb6f5e158d64b648d411c4a6dc1d
nicholaskunes <kunes.nick0@gmail.com>
2018-09-25 17:20:34 -0700

    Update millionaire.py

--- millionaire.py
+++ millionaire.py
@@ -1,64 +1,109 @@
 # -*- coding: utf-8 -*-
 from pymongo import MongoClient
 from bayesian_regression import *
+import subprocess
+import time
+import requests
+from datetime import datetime
+from tqdm import tqdm
 
 client = MongoClient()
 database = client['predictor']
 collection = database['gdax']
 
-# Retrieve price, v_ask, and v_bid data points from the database.
-prices = []
-v_ask = []
-v_bid = []
-num_points = 777600
-for doc in collection.find().limit(num_points):
-    prices.append(doc['price'])
-    v_ask.append(doc['v_ask'])
-    v_bid.append(doc['v_bid'])
+positive = 0
 
-# Divide prices into three, roughly equal sized, periods:
-# prices1, prices2, and prices3.
-[prices1, prices2, prices3] = np.array_split(prices, 3)
+while True:
+	prices = []
+	v_ask = []
+	v_bid = []
+	num_points = 777600
+	for doc in collection.find().limit(num_points):
+		prices.append(doc['price'])
+		v_ask.append(doc['v_ask'])
+		v_bid.append(doc['v_bid'])
 
-np.savetxt("prices3.csv", prices3, delimiter=",")
+	[prices1, prices2, prices3] = np.array_split(prices, 3)
+	[v_bid1, v_bid2, v_bid3] = np.array_split(v_bid, 3)
+	[v_ask1, v_ask2, v_ask3] = np.array_split(v_ask, 3)
+	
+	timeseries180 = generate_timeseries(prices1, 180)
+	timeseries360 = generate_timeseries(prices1, 360)
+	timeseries720 = generate_timeseries(prices1, 720)
+	
+	centers180 = find_cluster_centers(timeseries180, 100)
+	s1 = choose_effective_centers(centers180, 20)
 
-# Divide v_bid into three, roughly equal sized, periods:
-# v_bid1, v_bid2, and v_bid3.
-[v_bid1, v_bid2, v_bid3] = np.array_split(v_bid, 3)
+	centers360 = find_cluster_centers(timeseries360, 100)
+	s2 = choose_effective_centers(centers360, 20)
 
-# Divide v_ask into three, roughly equal sized, periods:
-# v_ask1, v_ask2, and v_ask3.
-[v_ask1, v_ask2, v_ask3] = np.array_split(v_ask, 3)
+	centers720 = find_cluster_centers(timeseries720, 100)
+	s3 = choose_effective_centers(centers720, 20)
 
-# Use the first time period (prices1) to generate all possible time series of
-# appropriate length (180, 360, and 720).
-timeseries180 = generate_timeseries(prices1, 180)
-timeseries360 = generate_timeseries(prices1, 360)
-timeseries720 = generate_timeseries(prices1, 720)
+	Dpi_r, Dp = linear_regression_vars(prices2, v_bid2, v_ask2, s1, s2, s3)
 
-# Cluster timeseries180 in 100 clusters using k-means, return the cluster
-# centers (centers180), and choose the 20 most effective centers (s1).
-centers180 = find_cluster_centers(timeseries180, 100)
-s1 = choose_effective_centers(centers180, 20)
+	w = find_parameters_w(Dpi_r, Dp)
 
-centers360 = find_cluster_centers(timeseries360, 100)
-s2 = choose_effective_centers(centers360, 20)
+	np.savetxt("btc.csv", dps, delimiter=",")
+	prices4 = []
+	iterator = 0
+	completion = 0
+    	position = 0
+	balance = 0
+	#for i in tqdm(range(0, 720, 1)): 
+	for i in range(0, 720, 1): 
+		completion += 1
+		prices = []
+		v_ask = []
+		v_bid = []
+		num_points = 777600
+		for doc in collection.find().limit(num_points):
+			prices.append(doc['price'])
+			v_ask.append(doc['v_ask'])
+			v_bid.append(doc['v_bid'])
 
-centers720 = find_cluster_centers(timeseries720, 100)
-s3 = choose_effective_centers(centers720, 20)
+		[prices1, prices2, prices3] = np.array_split(prices, 3)
+		[v_bid1, v_bid2, v_bid3] = np.array_split(v_bid, 3)
+		[v_ask1, v_ask2, v_ask3] = np.array_split(v_ask, 3)
 
-# Use the second time period to generate the independent and dependent
-# variables in the linear regression model:
-# Δp = w0 + w1 * Δp1 + w2 * Δp2 + w3 * Δp3 + w4 * r.
-Dpi_r, Dp = linear_regression_vars(prices2, v_bid2, v_ask2, s1, s2, s3)
+        end = predict(prices3, v_bid3, v_ask3, s1, s2, s3, w)
+		
+		ticker = requests.get('https://api.gdax.com/products/BTC-USD/ticker').json()
+		curprice = float(ticker['price'])
+		prices4.append(curprice)
+		
+		print "change_variables = [ price: " + str(curprice) + " Δp " + str(end) + " ]"
+		
+        	# BUY
+    		if end > 0.15 and position <= 0:
+			iterator += 1
+    			position += 1
+    		        balance -= curprice
+			print "[" + str(iterator) + " BUY] " + str(datetime.now()) + " predict t+10s Δp " + str(end) + " $" + str(round(balance, 5))
+        	# SELL
+    		if end < -0.15 and position >= 0:
+			iterator += 1
+    			position -= 1
+    			balance += curprice
+			print "[" + str(iterator) + " SELL] " + str(datetime.now()) + " predict t+10s Δp " + str(end) + " $" + str(round(balance, 5))
+		time.sleep(30)
+		
+	ticker = requests.get('https://api.gdax.com/products/BTC-USD/ticker').json()
 
-# Find the parameter values w (w0, w1, w2, w3, w4).
-w = find_parameters_w(Dpi_r, Dp)
+	# SELL
+    	if position == 1:
+        	balance += float(ticker['price'])
+    	# PAY BROKER BACK
+    	if position == -1:
+		balance -= float(ticker['price'])
+		
+	print "[series profit: $" + str(balance) + " ] " + "trade count: " + str(iterator)
+		
+	#np.savetxt("btc.csv", dps, delimiter=",")
+ 	np.savetxt("prices.csv", prices4, delimiter=",")
 
-# Predict average price changes over the third time period.
-dps = predict_dps(prices3, v_bid3, v_ask3, s1, s2, s3, w)
+ 	#output = subprocess.check_output("curl --upload-file ./btc.csv https://transfer.sh/btc.csv", shell=True)
+  	#output2 = subprocess.check_output("curl --upload-file ./prices.csv https://transfer.sh/prices.csv", shell=True)
 
-# What's your 'Fuck You Money' number?
-bank_balance = evaluate_performance(prices3, dps, t=0.35, step=1)
-
-print bank_balance
+ 	#subprocess.call("rm btc.csv", shell=True)
+	#subprocess.call("rm prices.csv", shell=True)
